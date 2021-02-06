153bdfcf008fa6c7e718f958970b7673b66f5ef7
John Winstead <jwinstead@launchdarkly.com>
2021-02-06 19:50:13 +0000

    try adding pointer lock

--- webapp/index.js
+++ webapp/index.js
@@ -1,66 +1,100 @@
+const SAMPLE_RATE = 20;
+
 const io = require('socket.io-client');
 const gameController = require('gamecontroller.js');
 const pointerLockPlus = require('pointer-lock-plus').default;
 
 let untilDomReady = new Promise((resolve)=>{document.addEventListener('DOMContentLoaded',()=>{resolve()});});
-
 let socket = io();
 
-const SAMPLE_RATE = 20;
+let caputre = false;
 let keys = {};
+let mouseButtons = {};
+
+let mouseXBuffer = 0;
+let mouseYBuffer = 0;
 
 untilDomReady.then(()=>{
     var button = document.createElement("button");
-    button.innerHTML = "Caputre Mouse Inputs";
+    button.innerHTML = "Caputre Inputs";
     var body = document.getElementsByTagName("body")[0];
     body.appendChild(button);
     button.addEventListener ("click", function() {
-        // alert("did something");
-        console.log("tryna pointer atain");
         pointerLockPlus({
             element: button,
-            onAttain: ()=>{console.log("pointer atain")},
-            onData: ()=>{console.log("pointer data")},
-            onClose: ()=>{console.log("pointer close")},
-            onRelease: ()=>{console.log("pointer release")},
-            onError: ()=>{console.log("pointer error")},
+            onAttain: ()=>{caputre = true},
+            onRelease: ()=>{caputre = false},
+            onData: (x, y)=>{
+                mouseXBuffer+=x;
+                mouseYBuffer+=y;
+            },
+
+            // onClose: ()=>{console.log("pointer close")},
+            // onError: ()=>{console.log("pointer error")},
         });
     });
+    window.addEventListener("mousedown", (e) => { mouseButton[e.which] = true; }, false);
+    window.addEventListener("mouseup", (e) => { mouseButton[e.which] = false; }, false);
 
     window.addEventListener("keydown", (e) => { keys[e.key] = true; }, false);
     window.addEventListener("keyup", (e) => { keys[e.key] = false; }, false);
 
     sync();
     function sync() {
+        let untilNextScheduledSample = new Promise((resolve, reject)=>{
+            setTimeout(()=>{
+                resolve()
+            }, 1000 / SAMPLE_RATE);
+        });
 
         let inputs = {};
-        inputs.keys = keys;
+        inputs.keys = {};
         inputs.controllers = {};
+        inputs.mouse = {};
 
-        if (gameController.gamepads) {
-            for(let gamepad of gameController.gamepads) {
-                inputs.controllers[gamepad.id] = {};
+        if (capture) {
+            inputs.keys = keys;
 
-                inputs.controllers[gamepad.id].buttons = {};
-                for (let buttonId = 0; buttonId < gamepad.buttons; buttonId++) {
-                    inputs.controllers[gamepad.id].buttons[buttonId] = gamepad.buttons[buttonId].pressed;
-                }
+            inputs.mouse = {
+                x: mouseXBuffer,
+                y: mouseYBuffer,
+                buttons: mouseButtons
+            }
 
-                inputs.controllers[gamepad.id].axes = {};
-                if (gamepad.axes) {
-                    const modifier = gamepad.axes.length % 2; // Firefox hack: detects one additional axe
-                    for (let x = 0; x < this.axes * 2; x++) {
-                    inputs.controllers[gamepad.id].axes[x] = gamepad.axes[x + modifier].toFixed(4);
+            // reset the mouse buffers
+            mouseXBuffer = 0;
+            mouseYBuffer = 0;
+
+            if (gameController.gamepads) {
+                for(let gamepad of gameController.gamepads) {
+                    inputs.controllers[gamepad.id] = {};
+
+                    inputs.controllers[gamepad.id].buttons = {};
+                    for (let buttonId = 0; buttonId < gamepad.buttons; buttonId++) {
+                        inputs.controllers[gamepad.id].buttons[buttonId] = gamepad.buttons[buttonId].pressed;
+                    }
+
+                    inputs.controllers[gamepad.id].axes = {};
+                    if (gamepad.axes) {
+                        const modifier = gamepad.axes.length % 2; // Firefox hack: detects one additional axe
+                        for (let x = 0; x < this.axes * 2; x++) {
+                        inputs.controllers[gamepad.id].axes[x] = gamepad.axes[x + modifier].toFixed(4);
+                        }
                     }
                 }
             }
         }
 
-        console.log(inputs);
-        socket.emit('inputs', inputs, (res) => {
-            console.log(res.inputs);
-            sync();
-            // setTimeout(sync, 1000 / SAMPLE_RATE);
+        // console.log(inputs);
+        let untilEmitCompletes = new Promise((resolve, reject)=>{
+            socket.emit('inputs', inputs, (res) => {
+                resolve();
+            });
         });
+
+        Promise.all([
+            untilNextScheduledSample,
+            untilEmitCompletes
+        ]).then(sync);
     }
 });
