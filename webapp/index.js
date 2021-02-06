206b3b3d7e282e34db921cd7d1f716965f773acf
John Winstead <jwinstead@launchdarkly.com>
2021-02-06 19:14:15 +0000

    try adding pointer lock

--- webapp/index.js
+++ webapp/index.js
@@ -1,3 +1,9 @@
+let untilDomReady = new Promise((resolve, reject) => {
+    document.addEventListener('DOMContentLoaded', (event) => {
+        resolve();
+    });
+});
+
 const SAMPLE_RATE = 20;
 
 const io = require('socket.io-client');
@@ -19,49 +25,45 @@ window.addEventListener('keyup',
     },
 false);
 
-sync();
-function sync() {
-    pointerLockPlus({
-        onAttain: ()=>{console.log("pointer atain")},
-        onData: ()=>{console.log("pointer data")},
-        onClose: ()=>{console.log("pointer close")},
-        onRelease: ()=>{console.log("pointer release")},
-        onError: ()=>{console.log("pointer error")},
-    });
+untilDomReady.then(()=>{
+    sync();
+    function sync() {
+        pointerLockPlus({
+            onAttain: ()=>{console.log("pointer atain")},
+            onData: ()=>{console.log("pointer data")},
+            onClose: ()=>{console.log("pointer close")},
+            onRelease: ()=>{console.log("pointer release")},
+            onError: ()=>{console.log("pointer error")},
+        });
 
-    let inputs = {};
-    inputs.keys = keys;
-    inputs.controllers = {};
+        let inputs = {};
+        inputs.keys = keys;
+        inputs.controllers = {};
 
-    if (gameController.gamepads) {
-        for(let gamepad of gameController.gamepads) {
-            inputs.controllers[gamepad.id] = {};
+        if (gameController.gamepads) {
+            for(let gamepad of gameController.gamepads) {
+                inputs.controllers[gamepad.id] = {};
 
-            inputs.controllers[gamepad.id].buttons = {};
-            for (let buttonId = 0; buttonId < gamepad.buttons; buttonId++) {
-                inputs.controllers[gamepad.id].buttons[buttonId] = gamepad.buttons[buttonId].pressed;
-            }
+                inputs.controllers[gamepad.id].buttons = {};
+                for (let buttonId = 0; buttonId < gamepad.buttons; buttonId++) {
+                    inputs.controllers[gamepad.id].buttons[buttonId] = gamepad.buttons[buttonId].pressed;
+                }
 
-            inputs.controllers[gamepad.id].axes = {};
-            if (gamepad.axes) {
-                const modifier = gamepad.axes.length % 2; // Firefox hack: detects one additional axe
-                for (let x = 0; x < this.axes * 2; x++) {
-                inputs.controllers[gamepad.id].axes[x] = gamepad.axes[x + modifier].toFixed(4);
+                inputs.controllers[gamepad.id].axes = {};
+                if (gamepad.axes) {
+                    const modifier = gamepad.axes.length % 2; // Firefox hack: detects one additional axe
+                    for (let x = 0; x < this.axes * 2; x++) {
+                    inputs.controllers[gamepad.id].axes[x] = gamepad.axes[x + modifier].toFixed(4);
+                    }
                 }
             }
         }
+
+        console.log(inputs);
+        socket.emit('inputs', inputs, (res) => {
+            console.log(res.inputs);
+            sync();
+            // setTimeout(sync, 1000 / SAMPLE_RATE);
+        });
     }
-
-    console.log(inputs);
-    socket.emit('inputs', inputs, (res) => {
-        console.log(res.inputs);
-        sync();
-        // setTimeout(sync, 1000 / SAMPLE_RATE);
-    });
-}
-
-window.addEventListener("gamepadconnected", function(e) {
-  console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
-    e.gamepad.index, e.gamepad.id,
-    e.gamepad.buttons.length, e.gamepad.axes.length);
 });
